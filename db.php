3804d1596c677aa7e9c46a729a782fca77ad1cbc
Ashok <ashokdey100@gmail.com>
2016-03-24 17:20:37 +0530

    Final Commit

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+    // Creating variables
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName ="mydb";
+
+    //connect to mySQL
+	$con = mysqli_connect($host, $dbUsername, $dbPassword, $dbName);
+
+    //testing connection
+    if (!$con){
+        die("Connection error: " . mysqli_connect_error());
+    }
+?>
